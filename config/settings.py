a6c05e2b91215c5d887411be90b1ebe90e670171
ftsvg <235974408+ftsvg@users.noreply.github.com>
2026-01-04 23:28:42 +0100

    refactor(database): use settings for database configuration

--- config/settings.py
+++ config/settings.py
@@ -8,8 +8,15 @@ from content import SETTINGS_ERRORS
 
 class Settings:
     TOKEN: str = getenv("TOKEN")
+    DBUSER: str = getenv("DBUSER")
+    DBPASS: str = getenv("DBPASS")
+    DBNAME: str = getenv("DBNAME")
+    DBENDPOINT: str = getenv("DBENDPOINT")
 
     @classmethod
     def validate(cls) -> None:
         if not cls.TOKEN:
             raise RuntimeError(SETTINGS_ERRORS['missing_token']['message'])
+        
+        if not all([cls.DBUSER, cls.DBPASS, cls.DBNAME, cls.DBENDPOINT]):
+            raise RuntimeError("Database credentials are missing.")
--- database/connection.py
+++ database/connection.py
@@ -1,46 +1,31 @@
-import os
 import functools
 import pymysql
 from pymysql.cursors import Cursor
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-def _get_database_credentials() -> tuple[str, str, str, str]:
-    db_username = os.getenv('DBUSER')
-    db_password = os.getenv('DBPASS')
-    db_name = os.getenv('DBNAME')
-    db_endpoint = os.getenv('DBENDPOINT')
-    
-    return db_username, db_password, db_name, db_endpoint
+from config import Settings
 
 
 def db_connect():
-    db_username, db_password, db_name, db_endpoint = _get_database_credentials()
-    conn = pymysql.connect(
-        host=db_endpoint,
+    return pymysql.connect(
+        host=Settings.DBENDPOINT,
         port=3306,
-        user=db_username,
-        password=db_password,
-        database=db_name,
+        user=Settings.DBUSER,
+        password=Settings.DBPASS,
+        database=Settings.DBNAME,
         autocommit=True
     )
-    return conn
 
 
 def ensure_cursor(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        cursor: Cursor | None = kwargs.get('cursor')
+        cursor: Cursor | None = kwargs.get("cursor")
         if cursor:
             return func(*args, **kwargs)
 
         with db_connect() as conn:
-            conn.autocommit = True
             cursor = conn.cursor()
-            kwargs['cursor'] = cursor
+            kwargs["cursor"] = cursor
             return func(*args, **kwargs)
 
     return wrapper
@@ -54,7 +39,6 @@ def async_ensure_cursor(func):
             return await func(*args, **kwargs)
 
         with db_connect() as conn:
-            conn.autocommit = True
             cursor = conn.cursor()
             kwargs['cursor'] = cursor
             return await func(*args, **kwargs)
